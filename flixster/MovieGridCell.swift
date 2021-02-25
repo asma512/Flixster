45ebb9b78d5c599af17b8735969f22bdb54897d1
Asma Sadia <asadia000@citymail.cuny.edu>
2021-02-25 13:58:31 -0500

    collection view setup

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Asma Sadia on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
